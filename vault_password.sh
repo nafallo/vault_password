f3ecaa8bcd367fe46f1fc2a9c3e5e6b3a5227838
Nafallo Bjälevik <nafallo@ubuntu.com>
2018-05-23 18:24:41 +0200

    Add author, license and usage information to script

--- vault_password.sh
+++ vault_password.sh
@@ -1,4 +1,22 @@
 #!/bin/dash
+# vault_password.sh by Nafallo Bjälevik <nafallo@ubuntu.com>
+# Released under the GNU GPL v3 licence.
+#
+# This script is supposed to be used as input to the
+# vault_password_file configuration setting in ansible.cfg
+#
+# You will still need a .vault_password file in your ansible
+# directory, but rather than only setting it static you can
+# set the first line to a service method and the last line
+# to a lookup that service supports, for example:
+#
+# lastpass
+# <name of a note stored in your vault>
+#
+# You can still specify a password directly in the file as
+# well, and if the file doesn't exist because you're not
+# using ansible vaults, we will output: password
+#
 if [ -s '.vault_password' ]; then
   if [ `head -n1 .vault_password` = "lastpass" ]; then
     lpass show --notes `tail -n1 .vault_password`
